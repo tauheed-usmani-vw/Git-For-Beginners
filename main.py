41fd4fcd7f6016b996f245a35bf36543f470e0a3
Tauheed Usmani <tauheedusmani07@gmail.com>
2026-07-26 09:33:01 +0530

    added divide_num feature in calculator.py and main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from utils.calculator import add_num
+from utils.calculator import add_num , divide_num
 
 print(add_num(2,3))
+
+print(divide_num(10,5))
--- utils/calculator.py
+++ utils/calculator.py
@@ -1,2 +1,5 @@
 def add_num(a,b):
     return a+b
+
+def divide_num(a,b):
+    return a/b
