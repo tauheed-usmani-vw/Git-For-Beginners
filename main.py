e07a7d1cacba4aa7b4d87692c5a24ea0f9cf6ebc
Tauheed Usmani <tauheed.usmani@vwgds.in>
2026-07-29 12:38:14 +0530

    Updated main.py with discription

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from utils.calculator import add_num , divide_num , subtract_num , multiply_num , power_num
 
-print(add_num(2,3))
+print("addition", add_num(2,3))
 
-print(divide_num(10,5))
+print("division", divide_num(10,5))
 
-print(subtract_num(5,3))
+print("subtraction", subtract_num(5,3))
 
-print(multiply_num(5,2))
+print("multiplication", multiply_num(5,2))
 
-print(power_num(2,4))
+print("power", power_num(2,4))
 
 
