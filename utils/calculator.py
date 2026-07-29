80887d32e79ae7cc5c49e4e44741e37adf7d1338
Tauheed Usmani <tauheed.usmani@vwgds.in>
2026-07-29 14:36:07 +0530

    updated string description in calculator.py module

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,14 +1,20 @@
 def add_num(a,b):
     return a + b
+#Returns the sum of a and b
 
 def divide_num(a,b):
     return a / b
+#Returns the divison result od a and b
 
 def subtract_num(a,b):
     return a - b
+#Return the subtraction of a and b
 
 def multiply_num(a,b):
     return a * b
+#Return the multiplication result of a and b
 
 def power_num(a,b):
     return a ** b
+
+#Return the power result of a to the power of b 
